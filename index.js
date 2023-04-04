0094aae67ec99c8ff55777bb7d68e69376af59db
Malcolm Wilson <malcolmwilsonmcg@gmail.com>
2023-04-04 01:13:12 +0100

    Updated DOM stickers variable, added delete func

--- index.js
+++ index.js
@@ -1,48 +1,64 @@
-const stickers = [
-  (star = document.getElementById("star")),
-  (fire = document.getElementById("fire")),
-  (heart = document.getElementById("heart")),
-  (thumbsup = document.getElementById("+1")),
-  (thumbsdown = document.getElementById("-1")),
-];
+// using DOM Manipulation, create stickers array
+const stickers = document.querySelectorAll(".sticker");
 
+// main function which begins sticker movement
 function makeDraggable(sticker) {
-  sticker.onclick = function (event) {
+  sticker.onclick = function (e) {
+    // when sticker is clicked, clone it in its current pos and create
+    // a parent element variable. Move it out of any current parents
+    // directly into body, make it positioned relative to the body &
+    // make clone draggable, appending it to parent element variable
     const clone = sticker.cloneNode(true);
     const stickerParent = sticker.parentElement;
-    // when sticker is clicked, move it out of any current parents directly into body
-    // to make it positioned relative to the body, and clone it in its current pos
     document.body.append(sticker);
+    stickerParent.append(clone);
+    makeDraggable(clone);
 
-    // centers the sticker at pageX and pageY (webpage) coordinates under the mouse
+    // centers the sticker at pageX and pageY (webpage) returning half of the
+    // width and height of the sticker using the values as coordinates under the mouse
     function centerSticker(pageX, pageY) {
       sticker.style.left = pageX - sticker.offsetWidth / 2 + "px";
       sticker.style.top = pageY - sticker.offsetHeight / 2 + "px";
     }
 
-    // call centerSticker whichmove sticker under the pointer, based on the
-    // event parameter which is attached to the mousedown
-    centerSticker(event.pageX, event.pageY);
+    // call centerSticker which moves sticker under middle of the
+    // pointer, taking the event parameter which is attached to the initial onclick event
+    centerSticker(e.pageX, e.pageY);
 
     // while the mouse is moving, perform the centerSticker callback function, which
-    // makes the sticker follow the mouse
-    function onMouseMove(event) {
-      centerSticker(event.pageX, event.pageY);
+    // makes the sticker follow the mouse while it is moving via onMouseMove
+    function onMouseMove(e) {
+      centerSticker(e.pageX, e.pageY);
     }
 
-    // perform the onMouseMove function while the mouse is moving
+    // use the onMouseMove callback function while the mouse is moving
     document.addEventListener("mousemove", onMouseMove);
 
-    // drop the sticker on button depress, make it unselectable also
-    sticker.onmouseup = function (e) {
+    // drop the sticker on button depress, make it unselectable
+    const dropSticker = document.addEventListener("mouseup", function () {
       document.removeEventListener("mousemove", onMouseMove);
       sticker.onmouseup = null;
       sticker.onclick = null;
-      stickerParent.append(clone);
-      makeDraggable(clone);
-    };
+    });
+
+    // create deleteIcon, append it to side of sticker
+    let deleteIcon = document.createElement("img");
+    deleteIcon.src = "assets/cross.png";
+    deleteIcon.style.height = "15px";
+    deleteIcon.style.position = "absolute";
+    deleteIcon.style.paddingTop = "9.5px";
+    sticker.append(deleteIcon);
+
+    // add event listener to deleteIcon
+    deleteIcon.addEventListener("click", deleteSticker);
+
+    // add "delete" class to deleted sticker and delete it
+    function deleteSticker(e) {
+      let currentSticker = e.target.sticker;
+      sticker.setAttribute("class", "delete");
+    }
   };
 }
 
-// Apply makeDraggable to all stickers
+// loop through stickers array, applying makeDraggable to all
 stickers.forEach(makeDraggable);
